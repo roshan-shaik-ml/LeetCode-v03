3b6edde57abba429e577a187d8d216e1ebc32c43
Shaik Faizan Roshan Ali <roshan.shaik.ml@gmail.com>
2026-03-19 07:13:39 +0530

    Refactor topKFrequent method for efficiency

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Using sorted with key and reverse along with a frequency map
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         freqMap = Counter(nums)
@@ -6,3 +7,9 @@ class Solution:
 
         sortedByFreq = sorted(freqMap.items(), key = lambda item : item[1], reverse = True)
         return [num for num, _ in sortedByFreq[:k]]
+
+    # Using counter's internal method 
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        
+        freqMap = Counter(nums)
+        return [num for num, freq in Counter(nums).most_common(k)]
